d1e8e5afd43cec07a7eb8910ca2e3e0ee9897b93
Duncan Reeves <duncansreeves@gmail.com>
2016-08-04 22:36:26 -0600

    Writeing the read file back

--- hello.js
+++ hello.js
@@ -5,11 +5,14 @@ http.createServer(function(request, response)
    response.writeHead(200);
    //response.write("<p>Hello world.</p><span>Stuff</span>");
    request.on('readable', function() {
-	let chunk = null;
-	while (null !== (chunk = request.read()));
+	var chunk = null;
+	while (null !== (chunk = request.read())) {
 	  response.write(chunk);
-	  response.end();
+	}
    });
+   request.on('end', function(){
+	response.end();
+});
 }).listen(8080);
 
 console.log('Listening on port 8080...');
