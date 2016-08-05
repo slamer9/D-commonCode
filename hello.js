a081f043be2b611a8da745b434e7d0dff6067b6d
Duncan Reeves <duncansreeves@gmail.com>
2016-08-04 21:51:50 -0600

    Node

--- hello.js
+++ hello.js
@@ -3,7 +3,12 @@ var http = require('http');
 http.createServer(function(request, response)
 {
    response.writeHead(200);
-   response.write("<p>Hello world.</p><span>Stuff</span>");
+   //response.write("<p>Hello world.</p><span>Stuff</span>");
+   request.on('readable', function() {
+	let chunk = null;
+	while (null !== (chunk = request.read()));
+	  response.write(chunk);
+   });
    response.end();
 }).listen(8080);
 
